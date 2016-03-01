6345693c07bcd7aa9dae193d5648020893f88f1c
johnfab <fabio.johner@stud.hslu.ch>
2016-03-01 10:26:08 +0100

    Platform.h created
    Platform.h created, pre commit for stefi

---
+++ RoboterSW/Intro_FRDM/Sources/Local_Platform.h
@@ -0,0 +1,14 @@
+/*
+ * Local_Platform.h
+ *
+ *  Created on: 01.03.2016
+ *      Author: fabio
+ */
+
+#ifndef LOCAL_PLATFORM_H_
+#define LOCAL_PLATFORM_H_
+
+#define LOCAL_PLATFORM_IS_FRDM (1)
+#define LOCAL_PLATFORM_IS_ROBO (0)
+
+#endif /* LOCAL_PLATFORM_H_ */
