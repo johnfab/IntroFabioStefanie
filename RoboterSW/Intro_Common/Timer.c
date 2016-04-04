70e0447d233c5c6afdf7a6eb1dee2e59191f723e
johnfab <fabio.johner@stud.hslu.ch>
2016-04-04 11:53:36 +0200

    Merged Common Folder Manually

--- RoboterSW/Intro_Common/Timer.c
+++ RoboterSW/Intro_Common/Timer.c
@@ -10,19 +10,23 @@
 #if PL_CONFIG_HAS_TIMER
 #include "Timer.h"
 #include "Event.h"
-#include "Trigger.h"
 
 void TMR_OnInterrupt(void) {
   /* this one gets called from an interrupt!!!! */
   /*! \todo Add code for a blinking LED here */
-  TRG_AddTick();		/* decrements ticks in callback array */
   static unsigned int cntr = 0;
-  #define BLINK_PERIOD_MS 2000
+  #define BLINK_PERIOD_MS 1000
   /* this one gets called from an interrupt!!!! */
   cntr++;
   if ((cntr%(BLINK_PERIOD_MS/TMR_TICK_MS))==0) { /* every two seconds */
     EVNT_SetEvent(EVNT_LED_HEARTBEAT);
   }
+
+  /* Add a tick to the Trigger Handling */
+#if PL_CONFIG_HAS_TRIGGER
+  TRG_AddTick();
+#endif
+
 }
 
 void TMR_Init(void) {
